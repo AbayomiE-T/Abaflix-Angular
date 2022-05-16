a1a6f6056b194cdb634863ec2ca0806c710b9816
Abayomi Adebowale <eyitouyoa3@gmail.com>
2022-05-16 14:56:18 -0400

    created movies reducer

---
+++ src/app/store/movies/movies.reducer.ts
@@ -0,0 +1,46 @@
+import { createReducer, on } from "@ngrx/store";
+import * as MovieActions from '../../store/movies/movies.actions'
+
+export interface State{
+    movies: any[],
+    likedMovies: any[],
+    selectedMovie: any;
+};
+
+const initialState: State = {
+    movies: [],
+    likedMovies: [],
+    selectedMovie: null
+};
+
+const movieReducer = createReducer(
+    initialState,
+    on(MovieActions.fetchMovies,
+        (state:State)=> ({
+            ...state
+        })),
+    
+    on(MovieActions.setMovies, 
+        (state: State, action)=> ({
+            ...state,
+            movies: action.movies
+        })),
+    
+    on(MovieActions.addToLikes,
+        (state: State, action)=> ({
+            ...state,
+            likedMovies: [...state.likedMovies, action.movie]
+        })),
+
+    on(MovieActions.findMovie,
+        (state: State, action)=>({
+            ...state,
+            selectedMovie: state.movies.filter((movie)=> movie.id === action.id)[0]
+        })),
+
+    on(MovieActions.removeFromLikes,
+        (state: State, action)=>({
+            ...state,
+            likedMovies: state.likedMovies.filter((movie)=> movie.id !== action.id);
+        }))
+)
