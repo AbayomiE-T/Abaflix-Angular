01bd144518767d2a3906c6b6cf9e998901e4e31e
Abayomi Adebowale <eyitouyoa3@gmail.com>
2022-05-16 13:32:40 -0400

    created components for movies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { MovieComponent } from './components/movie/movie.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { LikedMoviesComponent } from './components/liked-movies/liked-movies.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    MovieListComponent,
+    LikedMoviesComponent,
+    MovieDetailComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
