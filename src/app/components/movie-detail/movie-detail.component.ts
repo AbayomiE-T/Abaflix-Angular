b2e16a56ce2e8e8fe1497edadfb41109ad4155fb
Abayomi Adebowale <eyitouyoa3@gmail.com>
2022-05-17 20:25:04 -0400

    done (but with some minor bugs left)

--- src/app/components/movie-detail/movie-detail.component.ts
+++ src/app/components/movie-detail/movie-detail.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { IMovie } from 'src/app/interfaces/IMovie';
+import * as fromApp from '../../store/app.reducer';
+import * as MovieActions from '../../store/movies/movies.actions';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.scss']
 })
-export class MovieDetailComponent implements OnInit {
+export class MovieDetailComponent implements OnInit, OnDestroy {
+  public selectedMovie: IMovie | null;
+  public likedMovies: IMovie[];
+  private onDestroy$: Subject<void> = new Subject<void>();
+  public baseUrl = 'https://image.tmdb.org/t/p/original';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private store: Store<fromApp.AppState>, private location: Location) { }
 
   ngOnInit(): void {
+    const id:number = +this.route.snapshot.params['id'];
+
+    this.store.dispatch(MovieActions.findMovie({id}));
+    this.store.select('movies').pipe(takeUntil(this.onDestroy$)).subscribe((movies)=>{
+      this.selectedMovie = movies.selectedMovie;
+      this.likedMovies = movies.likedMovies;
+    })
   }
 
+  ngOnDestroy(): void{
+    this.onDestroy$.next();
+  }
+
+  public likeToggleHandler(): void {
+    if (!this.isLiked()) {
+      this.store.dispatch(MovieActions.addToLikes({movie: this.selectedMovie!}));
+  }
+
+  else {
+      this.store.dispatch(MovieActions.removeFromLikes({id: this.selectedMovie!.id}));
+  }
+
+  }
+
+  public goBack(): void{
+    this.location.historyGo(-1);
+  }
+
+  public isLiked(): boolean{
+    
+      return !!this.likedMovies.find((movie) => movie.id === this.selectedMovie!.id);
+  }
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import * as fromApp from '../store/app.reducer';
-import * as MovieActions from '../store/movies/movies.actions';
 
 const baseUrl = "https://api.themoviedb.org/3/movie/popular?api_key=d0f5f2e135336200362af8a1a73acb17";
 
@@ -12,7 +9,7 @@ const baseUrl = "https://api.themoviedb.org/3/movie/popular?api_key=d0f5f2e13533
 })
 export class MovieService {
 
-  constructor(private http: HttpClient, private store: Store<fromApp.AppState>) { }
+  constructor(private http: HttpClient) { }
 
   public getMovies(): Observable<any>{
     return this.http.get(baseUrl);
